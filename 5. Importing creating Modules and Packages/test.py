ac9f8d9b439eab72d193c8cd3c36f6b2ce582bbe
Abhishek <gaikwadabhishek1508@gmial.com>
2025-02-22 10:47:22 +0530

    Importing creating Modules and Packages folder added

---
+++ 5. Importing creating Modules and Packages/test.py
@@ -0,0 +1,6 @@
+from package.maths import addition, subtraction
+from package.subpackages.mult import multiplication
+
+print(addition(5, 19))
+print(subtraction(15, 4))
+print(multiplication(4, 8))
